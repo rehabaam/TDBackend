54a30702a00c3abdcfd31453924ede00e3d035b9
Ahmad Samra <rehabaam@gmail.com>
2020-11-14 17:34:28 +0200

    updating mod file

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
 module TDBackend
 
-go 1.14
+go 1.15
 
 require (
-	github.com/gorilla/mux v1.7.4
-	go.uber.org/zap v1.15.0
+	github.com/gorilla/mux v1.8.0
+	go.uber.org/zap v1.16.0
+	golang.org/x/tools v0.0.0-20201113202037-1643af1435f3 // indirect
 	gopkg.in/yaml.v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package tdbackend
 
 import (
 	"TDBackend/config"
@@ -6,15 +6,22 @@ import (
 	"fmt"
 )
 
+// main func loads application's configurations and starts the HTTP server
 func main() {
 
 	// Run HTTP server
 	config.Load()
+	startServer()
+
+	return
+}
+
+// startServer func starts an HTTP server that exposes RESTful APIs
+func startServer() error {
 	err := commands.RunServer()
 	if err != nil {
 		// Print the error occured while starting the server.
 		fmt.Printf("%+v\n", err.Error())
 	}
-	return
-
+	return err
 }
