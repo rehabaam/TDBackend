418718cde699ea1e83fbdcfc5318a357fdb1b943
Ahmad Samra <rehabaam@gmail.com>
2021-03-23 21:38:40 +0200

    renaming main file and updating labels

---
+++ TDWorker.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"TDBackend/config"
+	commands "TDBackend/services"
+	"fmt"
+)
+
+// main func loads application's configurations and starts the HTTP server
+func main() {
+
+	// Run HTTP server
+	config.Load()
+	startServer()
+
+	return
+}
+
+// startServer func starts an HTTP server that exposes RESTful APIs
+func startServer() error {
+	err := commands.RunServer()
+	if err != nil {
+		// Print the error occured while starting the server.
+		fmt.Printf("%+v\n", err.Error())
+	}
+	return err
+}
---
+++ localization/labels.go
@@ -0,0 +1,27 @@
+package labels
+
+const (
+
+	// General labels
+	Code       = "code"
+	RequestID  = "request-id"
+	HTTPScheme = "http-scheme"
+	HTTPProto  = "http-proto"
+	HTTPMethod = "http-method"
+	RemoteAddr = "remote-addr"
+	UserAgent  = "user-agent"
+	URI        = "uri"
+
+	// HTTPContentType label
+	HTTPContentTypeKey   = "Content-Type"
+	HTTPContentTypeValue = "application/json"
+
+	// logger levels
+	Info  = "info"
+	Debug = "debug"
+	Error = "error"
+
+	// Custom Millisecond for RC3339
+	RFC3339Milli = "2006-01-02T15:04:05.999Z07:00"
+
+)
