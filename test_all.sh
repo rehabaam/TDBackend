68f6eaa3c4e3af79e2abd8d430b7297f2d2c9ff4
Ahmad Samra <rehabaam@gmail.com>
2021-04-21 21:24:24 +0300

    fixing make file

--- test_all.sh
+++ test_all.sh
@@ -3,12 +3,5 @@
 set -e
 echo "" > coverage.out
 
-for d in $(go list ./... | grep -v vendor); do
-    echo -e "TESTS FOR: for \033[0;35m${d}\033[0m"
-    go test -race -v -coverprofile=profile.coverage.out -covermode=atomic $d
-    if [ -f profile.coverage.out ]; then
-        cat profile.coverage.out >> coverage.out
-        rm profile.coverage.out
-    fi
-    echo ""
-done
+echo -e "TESTS FOR: for \033[0;32mTDBackend\033[0m"
+go-acc -o coverage.out ./... -- -timeout 30m
