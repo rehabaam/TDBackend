0beecbac8d945d550521d1398f6bddeb8033c64f
Ahmad Samra <rehabaam@gmail.com>
2021-06-12 20:28:09 +0300

    adding FAQs to the list of APIs

--- services/commands_test.go
+++ services/commands_test.go
@@ -8,7 +8,7 @@ import (
 
 func Test_getSessions(t *testing.T) {
 
-	partner, _ := http.NewRequest("GET", "http://rehabaam.net:8080/TriDubai/v1/sessions/getSessions", nil)
+	data, _ := http.NewRequest("GET", "https://apps.tridubai.org/api/v1/sessions", nil)
 	rw := httptest.NewRecorder()
 
 	type args struct {
@@ -24,7 +24,7 @@ func Test_getSessions(t *testing.T) {
 			name: "getSessions_failure",
 			args: args{
 				w: rw,
-				r: partner,
+				r: data,
 			},
 			wantPanic: true,
 		},
@@ -44,7 +44,7 @@ func Test_getSessions(t *testing.T) {
 
 func Test_getDeals(t *testing.T) {
 
-	sessions, _ := http.NewRequest("GET", "http://rehabaam.net:8080/TriDubai/v1/sessions/getSessions", nil)
+	data, _ := http.NewRequest("GET", "https://apps.tridubai.org/api/v1/deals", nil)
 	rw := httptest.NewRecorder()
 
 	type args struct {
@@ -57,18 +57,18 @@ func Test_getDeals(t *testing.T) {
 		wantPanic bool
 	}{
 		{
-			name: "getSessions_failure",
+			name: "getDeals_failure",
 			args: args{
 				w: rw,
-				r: sessions,
+				r: data,
 			},
 			wantPanic: true,
 		},
 		{
-			name: "getSessions_success",
+			name: "getDeals_success",
 			args: args{
 				w: rw,
-				r: sessions,
+				r: data,
 			},
 			wantPanic: true,
 		},
@@ -78,7 +78,7 @@ func Test_getDeals(t *testing.T) {
 			defer func() {
 				r := recover()
 				if (r != nil) != tt.wantPanic {
-					t.Errorf("getSessions(), wantPanic = %v", tt.wantPanic)
+					t.Errorf("getDeals(), wantPanic = %v", tt.wantPanic)
 				}
 			}()
 			getDeals(tt.args.w, tt.args.r)
@@ -88,7 +88,7 @@ func Test_getDeals(t *testing.T) {
 
 func Test_getPartners(t *testing.T) {
 
-	partner, _ := http.NewRequest("GET", "http://rehabaam.net:8080/TriDubai/v1/sessions/getSessions", nil)
+	data, _ := http.NewRequest("GET", "https://apps.tridubai.org/api/v1/partners", nil)
 	rw := httptest.NewRecorder()
 
 	type args struct {
@@ -101,10 +101,10 @@ func Test_getPartners(t *testing.T) {
 		wantPanic bool
 	}{
 		{
-			name: "getSessions_failure",
+			name: "getPartner_failure",
 			args: args{
 				w: rw,
-				r: partner,
+				r: data,
 			},
 			wantPanic: true,
 		},
@@ -114,10 +114,82 @@ func Test_getPartners(t *testing.T) {
 			defer func() {
 				r := recover()
 				if (r != nil) != tt.wantPanic {
-					t.Errorf("getSessions(), wantPanic = %v", tt.wantPanic)
+					t.Errorf("getPartner(), wantPanic = %v", tt.wantPanic)
 				}
 			}()
 			getPartners(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func Test_getKit(t *testing.T) {
+
+	data, _ := http.NewRequest("GET", "https://apps.tridubai.org/api/v1/kit", nil)
+	rw := httptest.NewRecorder()
+
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantPanic bool
+	}{
+		{
+			name: "getKit_failure",
+			args: args{
+				w: rw,
+				r: data,
+			},
+			wantPanic: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				r := recover()
+				if (r != nil) != tt.wantPanic {
+					t.Errorf("getKit(), wantPanic = %v", tt.wantPanic)
+				}
+			}()
+			getKit(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func Test_getFAQs(t *testing.T) {
+
+	data, _ := http.NewRequest("GET", "https://apps.tridubai.org/api/v1/faqs", nil)
+	rw := httptest.NewRecorder()
+
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantPanic bool
+	}{
+		{
+			name: "getFAQs_failure",
+			args: args{
+				w: rw,
+				r: data,
+			},
+			wantPanic: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				r := recover()
+				if (r != nil) != tt.wantPanic {
+					t.Errorf("getFAQs(), wantPanic = %v", tt.wantPanic)
+				}
+			}()
+			getFAQs(tt.args.w, tt.args.r)
 		})
 	}
 }
