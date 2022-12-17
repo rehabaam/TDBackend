27510ee3350b26b26607408504ca7e6d4527c277
Ahmad Samra <rehabaam@gmail.com>
2022-12-17 09:57:05 +0200

    loading files into memory rather reading it every time

--- services/commands.go
+++ services/commands.go
@@ -8,36 +8,38 @@ import (
 
 // getPartners func for getting TriDubai annually partners
 func getPartners(w http.ResponseWriter, r *http.Request) {
-	_,_= readFileData("Partners", w, r)
+	_, _ = getData("Partners", w, r)
 }
 
 // getDeals func for getting TriDubai annually deals
 func getDeals(w http.ResponseWriter, r *http.Request) {
-	_,_= readFileData("Deals", w, r)
+	_, _ = getData("Deals", w, r)
 }
 
 // getSessions func for getting TriDubai weekly seesions
 func getSessions(w http.ResponseWriter, r *http.Request) {
-	_,_= readFileData("Sessions", w, r)
+	_, _ = getData("Sessions", w, r)
 }
 
 // getKit func for getting TriDubai Triathlon kit
 func getKit(w http.ResponseWriter, r *http.Request) {
-	_,_= readFileData("Kit", w, r)
+	_, _ = getData("Kit", w, r)
 }
 
 // getKit func for getting TriDubai Triathlon kit
 func getFAQs(w http.ResponseWriter, r *http.Request) {
-	_,_= readFileData("FAQs", w, r)
+	_, _ = getData("FAQs", w, r)
 }
 
 // serveImage func for serving TriDubai Session images
 func serveImage(w http.ResponseWriter, r *http.Request) {
-	_,_= getImage(w, r)
+	_, _ = getImage(w, r)
 }
 
 // RunServer func for running HTTP server
 func RunServer() error {
+
+	loadFileToMemory()
 	r := mux.NewRouter()
 
 	api := r.PathPrefix("/api/v1").Subrouter()
--- services/common.go
+++ services/common.go
@@ -12,8 +12,28 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// getFileData func for RESTful API
-func readFileData(endPoint string, w http.ResponseWriter, r *http.Request)(int, error) {
+var (
+	repo   = make(map[string]string)
+	fNames = []string{"Deals", "FAQs", "Kit", "Partners", "Sessions"}
+)
+
+// loadFileToMemory func loads data into memory for better response
+func loadFileToMemory() {
+
+	// Get current time
+	t := time.Now()
+
+	for _, v := range fNames {
+		data, err := readFile(v)
+		if err != nil {
+			logger.AppLogger(labels.Error, "Error while opening "+v+" file!", time.Since(t).Nanoseconds(), labels.Error+"|"+err.Error())
+		}
+		repo[v] = data
+	}
+}
+
+// readFile func reads data from files
+func readFile(endPoint string) (string, error) {
 
 	// Get current time
 	t := time.Now()
@@ -26,9 +46,8 @@ func readFileData(endPoint string, w http.ResponseWriter, r *http.Request)(int, 
 
 	// if we os.Open returns an error then handle it
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
 		logger.AppLogger(labels.Error, "Error while opening "+endPoint+" file!", time.Since(t).Nanoseconds(), labels.Error+"|"+err.Error())
-		return 0, err
+		return "", err
 	}
 
 	// defer the closing of our jsonFile so that we can parse it later on
@@ -38,23 +57,34 @@ func readFileData(endPoint string, w http.ResponseWriter, r *http.Request)(int, 
 	byteValue, errFile := ioutil.ReadAll(jsonFile)
 	if errFile != nil {
 		// Set HTTP code to 500
-		w.WriteHeader(http.StatusInternalServerError)
 		logger.AppLogger(labels.Error, "Error while reading "+endPoint+" file!", time.Since(t).Nanoseconds(), labels.Error+"|"+errFile.Error())
-		return 0, errFile
+		return "", errFile
 	}
+
+	var data string = string(byteValue)
+	logger.AppLogger(labels.Debug, "Data read from "+endPoint+" file!", time.Since(t).Nanoseconds(), labels.Data+"|"+data)
+
+	return data, nil
+}
+
+// getData func sends data back
+func getData(endPoint string, w http.ResponseWriter, r *http.Request) (int, error) {
+
+	// Get current time
+	t := time.Now()
 
 	// Set JSON as a Content-Type and User-Agent for output
 	w.Header().Add(labels.HTTPContentTypeKey, labels.HTTPContentTypeValue)
 	w.Header().Add(labels.HTTPUSERAGENTKey, labels.HTTPUSERAGENTValue)
 
-	logger.AppLogger(labels.Debug, "Result of GET func", time.Since(t).Nanoseconds(), labels.Code+"|"+fmt.Sprintf("%v", http.StatusOK))
+	logger.AppLogger(labels.Debug, "Result of GET "+endPoint+" func", time.Since(t).Nanoseconds(), labels.Code+"|"+fmt.Sprintf("%v", http.StatusOK))
 
 	// Send data out
-	return w.Write(byteValue)
+	return w.Write([]byte(repo[endPoint]))
 }
 
 // getImage func for Serving images
-func getImage(w http.ResponseWriter, r *http.Request) (int, error){
+func getImage(w http.ResponseWriter, r *http.Request) (int, error) {
 
 	// Get current time
 	t := time.Now()
